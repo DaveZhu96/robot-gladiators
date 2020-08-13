17286275d1997d501e3d1f2887ba2eb760efd239
DaveZhu96 <68628873+DaveZhu96@users.noreply.github.com>
2020-08-12 23:11:03 -0500

    one bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,7 +78,7 @@ var startGame = function() {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
+    
       var pickedEnemyObj = enemyInfo[i];
 
       pickedEnemyObj.health = randomNumber(40, 60);
@@ -188,8 +188,25 @@ var randomNumber = function(min, max) {
 
   return value;
 };
+// function to set name
 
+
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+// ***************************************
+
+  console.log("Your robot's name is" + name);
+  return name;
+};
 var playerInfo = {
+  name: getPlayerName(),
   name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
