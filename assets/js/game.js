8b51c66046599c89aa7c4bd66427e9f5c703300c
DaveZhu96 <68628873+DaveZhu96@users.noreply.github.com>
2020-08-13 00:07:38 -0500

    no idea what the F

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,26 +2,23 @@
 // You can also log multiple values at once like this
 
 
-
-
 var fightOrSkip = function() {
   // ask user if they'd like to fight or skip using  function
   var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-  // Conditional Recursive Function Call
-  promptFight = promptFight.toLowerCase();
-
-  if (promptFight === "skip") {
+ // Conditional Recursive Function Call
+if (promptFight === "" || promptFight === null) {
   window.alert("You need to provide a valid answer! Please try again.");
   return fightOrSkip();
 }
 
   // if user picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
     // confirm user wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-    // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping, but don't let them go into the negative
@@ -32,16 +29,16 @@ var fightOrSkip = function() {
     }
   }
 }
+
+
 var fight = function(enemy) {
   
   while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask user if they'd liked to fight or run
-   // ask user if they'd like to fight or skip using fightOrSkip function
-   if (fightOrSkip()) {
-    // if true, leave fight by breaking loop
-    break;
-  }
-
+    // ask user if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking loop
+      break;
+    }
     // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
     // generate random damage value based on player's attack power
 var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -151,8 +148,7 @@ var shop = function() {
   );
   // use switch to carry out action
 switch (shopOptionPrompt) {
-  case "REFILL":
-case "refill":
+  case 1:
   playerInfo.refillHealth();
   break;
   if (playerMoney >= 7) {
@@ -167,8 +163,7 @@ case "refill":
   }
 
   break;
-  case "UPGRADE":
-    case "upgrade":
+  case 2:
       playerInfo.upgradeAttack();
       break;
   if (playerMoney >= 7) {
@@ -183,8 +178,7 @@ case "refill":
   }
 
   break;
-  case "LEAVE": // new case
-  case "leave":
+  case 3:
     window.alert("Leaving the store.");
 
     // do nothing, so function will end
